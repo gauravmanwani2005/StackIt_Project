e1a6d01c90e59947b554ac25565106b86f1d6d14
Gaurav-Joshi-12 <gauravtechnicalwork@gmail.com>
2025-07-12 11:25:05 +0530

    Login Reg html css done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'login-s',
+    component:LoginComponent
+  },
+  {
+    path:'register-s',
+    component:RegisterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+  
+}
